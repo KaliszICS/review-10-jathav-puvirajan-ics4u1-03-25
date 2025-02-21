cd6f4002749fc90cd8a02b9dbbd1065ed891f338
jathav-puvirajan-ics4u1-03-25 <jathav.puvirajan@student.tdsb.on.ca>
2025-02-21 18:32:15 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,50 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.print(square(3));
 	}
-
+	public static String pyramid(int num){
+		String pyr = "";
+		for(int i = 0; i < num; i++){
+			if(i != 0)	pyr = pyr + "\n";
+			for(int j = 0; j < (num - i - 1); j++){
+				pyr = pyr + ' ';
+			}
+			for(int j = 0; j<i; j++){
+				pyr = pyr + "* ";
+			}
+			pyr = pyr + "*";
+		}
+		return pyr;
+	}
+	public static String square(int num){
+		String sqr = "";
+		for(int i = 0; i < num; i++){
+			if(i == num - 1){
+				for(int j = 0; j < num; j++){
+					sqr = sqr + "*";
+				}
+			}else if(i == 0){
+				for(int j = 0; j < num; j++){
+					sqr = sqr + "*";
+				}
+				sqr = sqr + "\n";
+			}else{
+				sqr = sqr + "*";
+				for(int j = 1; j < num-1; j++){
+					sqr = sqr + " ";
+				}
+				sqr = sqr + "*\n";
+			}
+		}
+		return sqr;
+	}
+	public static boolean hasLowercase(String str){
+		for(int i = 0; i < str.length();i++){
+			if(Character.isLowerCase(str.charAt(i))){
+				return true;
+			}
+		}
+		return false;
+	}
 }
